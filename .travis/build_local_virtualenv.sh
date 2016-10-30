69b1f63353f089bd7ba85eae2822e437d5a3b686
ukBaz <barry_byford@yahoo.co.uk>
2016-10-30 16:33:52 +0000

    Remove system_site_packages from Travis

--- .travis/build_local_virtualenv.sh
+++ .travis/build_local_virtualenv.sh
@@ -2,13 +2,15 @@
 # mkdir travis27
 # cd travis27/
 # virtualenv python2.7
-
+# source python2.7/bin/activate
 git clone https://github.com/ukBaz/python-bluezero
-python-bluezero/.travis/install_dbus.sh
-python-bluezero/.travis/install_dbusmock.sh
+cd python-bluezero/
+git checkout test_coverage
+# cd ..
+.travis/install_dbus.sh
+.travis/install_dbusmock.sh
 pip install pycodestyle
 pip install coverage
 pip install codeclimate-test-reporter
-git clone https://github.com/ukBaz/python-bluezero
 
 # deactive
