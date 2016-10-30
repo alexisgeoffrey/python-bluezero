cc2ed9a69efab1af998bbf187f406414c8e228cd
ukBaz <barry_byford@yahoo.co.uk>
2016-10-30 09:16:35 +0000

    Variables for install dirs

--- install_dbus.sh
+++ install_dbus.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 set -ex
 
-pwd
-which python
-find . -name site-packages
+SITE_PACKAGES=`pip --version | awk '{ print $4 }'`
+PYTHON_LOC=`which python`
+PYTHON_DIR=`echo ${PYTHON_LOC%/*/*}`
 
 if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ]; then
     apt-get download python-dbus
     dpkg -x python-dbus*.deb ./dbus
-    mv ./dbus/usr/lib/python2.7/dist-packages/* ./python_2.7/lib/python2.7/site-packages/
-    mv ./dbus/usr/share python_2.7/
+    mv ./dbus/usr/lib/python2.7/dist-packages/* $SITE_PACKAGES/
+    mv ./dbus/usr/share $PYTHON_DIR/
 
     apt-get download python-gi
     dpkg -x python-gi*.deb ./gi
-    mv ./gi/usr/lib/python2.7/dist-packages/* ./python_2.7/lib/python2.7/site-packages/
+    mv ./gi/usr/lib/python2.7/dist-packages/* $SITE_PACKAGES/
 fi
 
 if [ "$TRAVIS_PYTHON_VERSION" == "3.4" ]; then
     apt-get download python3-dbus
     dpkg -x python3-dbus*.deb ./dbus
-    mv ./dbus/usr/lib/python3/dist-packages/* ./python_3.4/lib/python3.4/site-packages/
-    mv ./dbus/usr/share python_3.4/
+    mv ./dbus/usr/lib/python3/dist-packages/* $SITE_PACKAGES/
+    mv ./dbus/usr/share $PYTHON_DIR/
 
     apt-get download python3-gi
     dpkg -x python3-gi*.deb ./gi
-    mv ./gi/usr/lib/python3/dist-packages/* ./python_3.4/lib/python3.4/site-packages/
+    mv ./gi/usr/lib/python3/dist-packages/* $SITE_PACKAGES/
 fi
